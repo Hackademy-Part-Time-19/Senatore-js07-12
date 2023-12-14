4ff73630f11cee4c74a0e44e717b17680940e319
MattiaSen <147874985+MattiaSen@users.noreply.github.com>
2023-12-14 14:43:38 +0100

    cars3

--- 0711.js
+++ 0711.js
@@ -1,39 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cards = document.querySelectorAll(".cards");
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".cards");
 
-    cards.forEach(function(card) {
-      card.addEventListener("mouseover", function() {
-        card.style.backgroundColor = "white";
-        card.style.color = "black";
-        const titoloCar = card.querySelector(".TitoloCar");
-        const parCar = card.querySelectorAll(".caratteristiche");
+  cards.forEach(function (card) {
+      card.addEventListener("mouseover", function () {
+          card.style.backgroundColor = "white";
+          card.style.color = "black";
+          const titoloCar = card.querySelector(".TitoloCar");
+          const parCar = card.querySelectorAll(".caratteristiche");
+          const Flexbtn = card.querySelectorAll(".btnColor");
 
-        if (titoloCar) {
-          titoloCar.style.color = "black";
-        }
+          if (titoloCar) {
+              titoloCar.style.color = "black";
+          }
 
-        if (parCar) {
-          parCar.forEach(function(par) {
-            par.style.color = "black";
-          });
-        }
+          if (parCar) {
+              parCar.forEach(function (par) {
+                  par.style.color = "black";
+              });
+          }
+          if (Flexbtn) {
+              Flexbtn.forEach(function (btn) {
+                  btn.style.borderColor = "black";
+              });
+          }
       });
 
-      card.addEventListener("mouseout", function() {
-        card.style.backgroundColor = "";
-        card.style.color = ""; 
-        const titoloCar = card.querySelector(".TitoloCar");
-        const parCar = card.querySelectorAll(".caratteristiche");
+      card.addEventListener("mouseout", function () {
+          card.style.backgroundColor = "";
+          card.style.color = "";
+          const titoloCar = card.querySelector(".TitoloCar");
+          const parCar = card.querySelectorAll(".caratteristiche");
+          const Flexbtn = card.querySelectorAll(".btnColor");
 
-        if (titoloCar) {
-          titoloCar.style.color = ""; 
-        }
+          if (titoloCar) {
+              titoloCar.style.color = "";
+          }
 
-        if (parCar) {
-          parCar.forEach(function(par) {
-            par.style.color = ""; 
-          });
-        }
+          if (parCar) {
+              parCar.forEach(function (par) {
+                  par.style.color = "";
+              });
+          }
+          if (Flexbtn) {
+              Flexbtn.forEach(function (btn) {
+                  btn.style.borderColor = "";
+              });
+          }
       });
-    });
   });
+});
